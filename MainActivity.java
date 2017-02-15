e4cfd776914ab4e29d0739cc755248729354412f
mpenny2 <mpenny2@nd.edu>
2017-02-15 16:12:26 -0500

    Lab2

--- MainActivity.java
+++ MainActivity.java
@@ -8,7 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,17 +20,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        String[] teams = {"Ohio State", "Florida State", "Wake Forest", "Boston College", "North Carolina State", "Georgia Tech","North Virginia", "Chicago State"};
+        String[] dates = {"Feb. 11", "Feb.14" ,"Feb.18", "Feb. 26", "Mar. 1", "Mar. 4", "Mar. 7", "Mar. 16"};
+
+        String[] osu = {Integer.toString(R.drawable.osu), "Ohio State", "Feb, 11"};
+        String[] fsu = {Integer.toString(R.drawable.fsu), "Florida State", "Feb, 14"};
+        String[] wf = {Integer.toString(R.drawable.wf), "Wake Forest", "Feb, 18"};
+        String[] bc = {Integer.toString(R.drawable.bc), "Boston College", "Feb, 26"};
+        String[] ncsu = {Integer.toString(R.drawable.ncsu), "North Carolina State", "Mar. 1"};
+        String[] gt = {Integer.toString(R.drawable.gt), "Georgia Tech", "Mar. 4"};
+        String[] nv = {Integer.toString(R.drawable.nova), "North Virginia", "Mar. 7"};
+        String [] cs = {Integer.toString(R.drawable.chicagost), "Chicago State", "Mar. 17"};
+
+        ArrayList info = new ArrayList();
+        info.add(osu);
+        info.add(fsu);
+        info.add(wf);
+        info.add(bc);
+        info.add(ncsu);
+        info.add(gt);
+        info.add(nv);
+        info.add(cs);
+        /*ArrayList info = new ArrayList();
+        info.add(teams);
+        info.add(dates);*/
+       /* dates.add("Feb. 11");
+        dates.add("Feb. 14");
+        dates.add("Feb. 18");
+        dates.add("Feb. 26");
+        dates.add("Mar. 1");
+        dates.add("Mar. 4");
+        dates.add("Mar. 7");
+        dates.add("Mar. 16");*/
+
+        ScheduleAdapter scheduleAdapter = new ScheduleAdapter(this, info);
+        ListView scheduleListView = (ListView) findViewById(R.id.scheduleListView);
+        scheduleListView.setAdapter(scheduleAdapter);
+
     }
 
     @Override
